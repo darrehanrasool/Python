5645a0db834ee630b9ca410235dd8d09e15808b7
darrehanrasool <darrehanrasool@gmail.com>
2026-08-28 15:33:34 +0530

    Setting Jupyter Note Book
    دار ریحان رسول

--- 0-rectangle.py
+++
@@ -1,5 +0,0 @@
-## Area of Rectangle
-length=10
-breadth=15
-area=length*breadth
-print('area = ',area)
--- 1-billamount.py
+++
@@ -1,8 +0,0 @@
-## Bill Amount 
-prices=[10,7,5,8,6]
-quantity=[1,2,3,1,1]
-total=0
-for i in range(len(prices)):
-    total=total+prices[i]*quantity[i]
-
-print(total)
