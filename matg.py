af53ce82f8be6898f028f80b24a30cfaace8f9f1
darrehanrasool <darrehanrasool@gmail.com>
2026-09-18 12:27:00 +0530

    دار ریحان رسول
    دار ریحان رسول

---
+++ matg.py
@@ -0,0 +1,47 @@
+import math
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# QUESTION 9
+# sin²(x) + cos²(x)
+
+x9 = math.radians(45)
+
+answer9 = math.sin(x9)**2 + math.cos(x9)**2
+print("Q9 =", answer9)
+
+
+
+# QUESTION 10
+# [sin(x) + cos(x)] / [sin(x) - cos(x)]
+
+x10 = math.radians(60)
+
+answer10 = (math.sin(x10) + math.cos(x10)) / (math.sin(x10) - math.cos(x10))
+
+print("Q10 =", answer10)
+
+
+
